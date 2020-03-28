89b46f345810c37c32503caaa6eed903b0e7018b
Taimir Aguacil <aguaciltaimir@gmail.com>
2020-03-28 21:46:10 +0100

    revert directory

--- analytics/webapp_core.py
+++ analytics/webapp_core.py
@@ -20,9 +20,9 @@ from typing import Union
 from sanic import Sanic
 
 # User-defined library import
-from analytics import webapp_settings
-from analytics.app.app_core import app_endpoints
-from analytics.Utilities.logger import create_logger
+webapp_settings
+app.app_core import app_endpoints
+Utilities.logger import create_logger
 
 logger = create_logger(__name__)
 
