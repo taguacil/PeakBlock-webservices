597d809c7ad46ea6c8fa37151cb581b55e0f9a8e
Yasmine Abdulfattah <yasmine.abdulfattah@ideainmotion.org>
2020-03-29 22:47:38 +0200

    Adds db connection string to env

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const winston = require("winston");
+const mongoose = require('mongoose');
+const config = require('config');
+const winston = require('winston');
 
 module.exports = async function() {
-  const db = "mongodb://localhost/PeakBlock";
-  mongoose.set("useNewUrlParser", true);
-  mongoose.connect(db).then(() => winston.info(`Connect to ${db} .....`));
+    // const db = 'mongodb://localhost/PeakBlock';
+    const db = config.get('db');
+    mongoose.set('useNewUrlParser', true);
+    mongoose.connect(db).then(() => winston.info(`Connect to ${db} .....`));
 };
