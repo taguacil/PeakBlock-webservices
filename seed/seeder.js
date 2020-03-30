c9192868ca22ddc54d5cc65244a4949f4deca1c7
Yasmine Abdulfattah <yasmine.abdulfattah@ideainmotion.org>
2020-03-30 14:38:49 +0200

    link cities with patients

--- seed/seeder.js
+++ seed/seeder.js
@@ -34,87 +34,94 @@ const conditions = ['Chronic cardiac disease  including congenital heart disease
 
 (async function () {
     try {
-        fs.readFile('./seed/patients.json', 'utf8', async (err, jsonString) => {
-            if (err) {
-                console.log('File read failed:', err);
+        fs.readFile('./seed/swiss-cities.json', 'utf8', async (err2, jsonString2) => {
+            if (err2) {
+                console.log('File read failed:', err2);
                 return;
             }
-            const patients = JSON.parse(jsonString);
-
-            await Patient.remove({}); // clear all users
-
-            for (let i = 0; i < patients.length; i += 1) {
-                const salt = await bcrypt.genSalt(10);
-                const password = await bcrypt.hash('123456789', salt);
-                const email = faker.internet.exampleEmail();
-                const name = `${faker.name.firstName()}${faker.name.lastName()}`;
-                const covid_probability = patients[i].Covid === 'YES' ? 1 : 0;
-                const gender = patients[i]['Sex at Birth:'] === 'Male' ? 'male' : 'female';
-
-                const age_years = Number(patients[i]['Estimated Age (years)']);
-                const date = new Date();
-                date.setFullYear(date.getFullYear() - age_years);
-                const birthDate = new Date(date);
-                const medical_conditions = [];
-
-                for (let j = 0; j < conditions.length; j += 1) {
-                    if (patients[i][conditions[j]] === 'YES') medical_conditions.push(conditions[j]);
-                }
-
-                const observations = {
-                    body_temperature: patients[i]['Temperature - Unit:'],
-                    cough: patients[i]['Cough - bloody sputum: haemoptysis'] === 'YES',
-                    fatigue: patients[i]['Fatigue '][' Malaise'] === 'YES',
-                    pain_in_throat: patients[i]['Sore throat'] === 'YES',
-                    dyspnea_at_rest: patients[i]['Shortness of breath (Dyspnea)'] === 'YES',
-                    headache: patients[i]['Headache'] === 'YES',
-                    diarrhea: patients[i]['Diarrhoea'] === 'YES',
-                    nausea: patients[i]['Vomiting '][' Nausea'] === 'YES',
-                    // loss_of_sense_of_smell: patients[i][''] === 'YES',
-                    chest_pain: patients[i]['Chest pain'] === 'YES',
-                    muscle_ache: patients[i]['Muscle aches (Myalgia)'] === 'YES',
-                    runny_nose: patients[i]['Runny nose (Rhinorrhoea)'] === 'YES',
-                    confusion: patients[i]['Altered consciousness']['confusion'] === 'YES',
+            const cities = JSON.parse(jsonString2);
+            console.log('start cities');
+            const citynames = [];
+            await City.remove({}); // clear all users
+            for (let i = 0; i < cities.length; i += 1) {
+                const cityObj = {
+                    name: cities[i].AccentCity,
+                    countryCode: cities[i].Country,
+                    lat: cities[i].Latitude,
+                    lon: cities[i].Longitude,
+                    population: cities[i].Population,
                 };
-
-                const patientObj = {
-                    name,
-                    password,
-                    email,
-                    covid_probability,
-                    gender,
-                    birthDate,
-                    medical_conditions,
-                    observations,
-                };
-
-                const patient = new Patient(patientObj);
-                await patient.save();
+                citynames.push(cities[i].AccentCity);
+                const city = new City(cityObj);
+                await city.save();
                 console.log('done');
             }
 
-            fs.readFile('./seed/swiss-cities.json', 'utf8', async (err2, jsonString2) => {
+            fs.readFile('./seed/patients.json', 'utf8', async (err, jsonString) => {
                 if (err) {
-                    console.log('File read failed:', err2);
+                    console.log('File read failed:', err);
                     return;
                 }
-                const cities = JSON.parse(jsonString2);
-                console.log('start cities');
-                await City.remove({}); // clear all users
-                for (let i = 0; i < cities.length; i += 1) {
-                    const cityObj = {
-                        name: cities[i].AccentCity,
-                        countryCode: cities[i].Country,
-                        lat: cities[i].Latitude,
-                        lon: cities[i].Longitude,
-                        population: cities[i].Population,
+                const patients = JSON.parse(jsonString);
+
+                await Patient.remove({}); // clear all users
+
+                for (let i = 0; i < patients.length; i += 1) {
+                    const salt = await bcrypt.genSalt(10);
+                    const password = await bcrypt.hash('123456789', salt);
+                    const email = faker.internet.exampleEmail();
+                    const name = `${faker.name.firstName()}${faker.name.lastName()}`;
+                    const covid_probability = patients[i].Covid === 'YES' ? 1 : 0;
+                    const gender = patients[i]['Sex at Birth:'] === 'Male' ? 'male' : 'female';
+
+                    const age_years = Number(patients[i]['Estimated Age (years)']);
+                    const date = new Date();
+                    date.setFullYear(date.getFullYear() - age_years);
+                    const birthDate = new Date(date);
+                    const medical_conditions = [];
+
+                    for (let j = 0; j < conditions.length; j += 1) {
+                        if (patients[i][conditions[j]] === 'YES') medical_conditions.push(conditions[j]);
+                    }
+
+                    const observations = {
+                        body_temperature: patients[i]['Temperature - Unit:'],
+                        cough: patients[i]['Cough - bloody sputum: haemoptysis'] === 'YES',
+                        fatigue: patients[i]['Fatigue '][' Malaise'] === 'YES',
+                        pain_in_throat: patients[i]['Sore throat'] === 'YES',
+                        dyspnea_at_rest: patients[i]['Shortness of breath (Dyspnea)'] === 'YES',
+                        headache: patients[i].Headache === 'YES',
+                        diarrhea: patients[i].Diarrhoea === 'YES',
+                        nausea: patients[i]['Vomiting '][' Nausea'] === 'YES',
+                        // loss_of_sense_of_smell: patients[i][''] === 'YES',
+                        chest_pain: patients[i]['Chest pain'] === 'YES',
+                        muscle_ache: patients[i]['Muscle aches (Myalgia)'] === 'YES',
+                        runny_nose: patients[i]['Runny nose (Rhinorrhoea)'] === 'YES',
+                        confusion: patients[i]['Altered consciousness'].confusion === 'YES',
                     };
 
-                    const city = new City(cityObj);
-                    await city.save();
+                    const city = citynames[Math.floor(Math.random() * citynames.length)];
+
+                    const patientObj = {
+                        name,
+                        password,
+                        email,
+                        covid_probability,
+                        gender,
+                        birthDate,
+                        medical_conditions,
+                        observations,
+                        address: {
+                            city,
+                        },
+                    };
+
+                    const patient = new Patient(patientObj);
+                    await patient.save();
                     console.log('done');
                 }
                 mongoose.connection.close();
+                console.log('done all');
             });
         });
     } catch (err) {
