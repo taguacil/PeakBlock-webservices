7ef1ca859bac189e865c9bf78f7d5d9cf3f3e733
Yasmine Abdulfattah <yasmine.abdulfattah@ideainmotion.org>
2020-03-30 02:17:57 +0200

    Adds organization endpoints

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
-var emailvalidator = require('email-validator');
-const { Organization } = require('../models/organization');
-const { Practitioner } = require('../models/practitioner');
+const emailvalidator = require('email-validator');
+const { Organization, validateRegistration } = require('../models/organization');
+const { Practitioner, validateRegistration: validatePractitioner } = require('../models/practitioner');
 const { PractitionerRole } = require('../models/practitionerRole');
 const { HealthcareService } = require('../models/healthcareService');
 
@@ -29,9 +29,8 @@ exports.login = async function authenticate(req, res, next) {
 
 // Organization Registration.
 exports.register = async (req, res) => {
-    // TODO:
-    // const error = validate(req.body);
-    // if (error) return res.status(400).send(error);
+    const error = validateRegistration(req.body);
+    if (error) return res.status(400).send(error);
 
     const valid = emailvalidator.validate(req.body.email);
     if (!valid) return res.status(400).send('Invalid Email!');
@@ -41,20 +40,81 @@ exports.register = async (req, res) => {
 
     organization = new Organization(
         _.pick(req.body, [
-        'name',
-        'email',
-        'password',
-        ])
+            'name',
+            'email',
+            'password',
+        ]),
     );
     const salt = await bcrypt.genSalt(10);
     organization.password = await bcrypt.hash(organization.password, salt);
     await organization.save();
 
-    res.send(_.pick(organization, ['_id', 'name', 'email']));
+    return res.send(_.pick(organization, ['_id', 'name', 'email']));
 };
 
 exports.get = async (req, res) => {
     if (!(req.user && req.user.role === 'organization')) return res.status(403).send({ message: 'You are not authorized' });
     const organization = await Organization.findById(req.user.id);
-    return res.send(organization)
+    return res.send(organization);
+};
+
+
+exports.addPractitoner = async (req, res) => {
+    if (!(req.user && req.user.role === 'organization')) return res.status(403).send({ message: 'You are not authorized' });
+    // TODO: data validation
+    let practitioner = await Practitioner.findOne({ email: req.body.email });
+    if (practitioner) return res.status(400).send('Email already registered.');
+
+    practitioner = new Practitioner(
+        _.pick(req.body, [
+            'name',
+            'email',
+            'password',
+        ]),
+    );
+    const salt = await bcrypt.genSalt(10);
+    practitioner.password = await bcrypt.hash(practitioner.password, salt);
+    await practitioner.save();
+
+    return res.send(_.pick(practitioner, ['_id', 'name', 'email']));
+};
+
+exports.addPractitonerRole = async (req, res) => {
+    if (!(req.user && req.user.role === 'organization')) return res.status(403).send({ message: 'You are not authorized' });
+    // TODO: data validation
+    const practitioner = await Practitioner.findById(req.body.practitioner);
+    if (!practitioner) return res.status(404).send({ message: 'Such practitioner does not exist. Please add pracititioner, then add the role!' });
+    const healthcareService = await HealthcareService.findById(req.body.healthcareService);
+    if (!healthcareService) return res.status(404).send({ message: 'Such healthcare service does not exist. Please add pracititioner, then add the role!' });
+
+    req.body.organization = req.user.id;
+    req.body.practitioner = req.params.practitioner;
+    const practitionerRole = new PractitionerRole(req.body);
+    await practitionerRole.save();
+
+    res.send(practitionerRole);
+};
+
+exports.addHealthcareService = async (req, res) => {
+    if (!(req.user && req.user.role === 'organization')) return res.status(403).send({ message: 'You are not authorized' });
+    // TODO: data validation
+    req.body.providedBy = req.user.id;
+    const healthcareService = new HealthcareService(req.body);
+    await healthcareService.save();
+
+    res.send(healthcareService);
+};
+
+exports.viewPractitioners = async (req, res) => {
+    if (!(req.user && req.user.role === 'organization')) return res.status(403).send({ message: 'You are not authorized' });
+    const practitioners = await PractitionerRole.find({ organization: req.user.id })
+        .select('practitioner')
+        .populate(
+            {
+                path: 'practitioner',
+                model: Practitioner,
+            },
+        );
+
+    res.send(practitioners);
 };
--- routes/organization.js
+++ routes/organization.js
@@ -6,5 +6,8 @@ const router = express.Router();
 router.post('/login', organizationController.login);
 router.post('/', organizationController.register);
 router.get('/', organizationController.get)
+router.post('/addPractitioner', organizationController.addPractitoner);
+router.post('/addHealthcareService', organizationController.addHealthcareService);
+router.post('/addRole', organizationController.addPractitonerRole);
 
 module.exports = router;
